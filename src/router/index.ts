e5ab75243e0ac965a3302a66f061664415e814a5
Edovanie Hiramis <paneedovanie@yahoo.com>
2022-10-22 22:35:42 +0800

    Removed unused code

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,23 +6,6 @@ import errorRoutes from './error'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [publicRoutes, userRoutes, errorRoutes]
-  // routes: [
-  //   {
-  //     path: "/",
-  //     name: "public-page",
-  //     component: PublicPage,
-  //   },
-  //   {
-  //     path: "/weather",
-  //     name: "weather-page",
-  //     component: () => import("../pages/WeatherPage.vue"),
-  //   },
-  //   // # Redirect user to home/landing page when accessing unknown route
-  //   {
-  //     path: "/:pathMatch(.*)",
-  //     redirect: "/",
-  //   },
-  // ],
 });
 
 export default router;
